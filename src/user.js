f981578253c5b767df2dc5e2fb6c17d618a50d77
Redix <redixra@ad.vzwcorp.com>
2021-11-24 07:21:15 -0600

    completing first steps of project

---
+++ src/user.js
@@ -0,0 +1,21 @@
+class User {
+    constructor(name, birthdate) {
+        this.name = name;
+        this.birthdate = birthdate;
+        this.payment
+    };
+
+    /*assignToScooter(name) {
+        return 
+
+    };
+
+    makePayment() {
+        return 'Thank you for your payment';
+    };*/
+};
+
+const user1 = new User('janet smith')
+
+console.log(user1);
+module.exports = User;
